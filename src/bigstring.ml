c9b0d704e05c4ed88f4105babab964deb215bc7e
Vincent Bernardoff <vb@luminar.eu.org>
2018-04-23 12:22:34 +0200

    move unix related functions to a opam package

--- src/bigstring.ml
+++ src/bigstring.ml
@@ -391,23 +391,3 @@ let lines s = split ~by:'\n' s
   empty |> lines = []
   of_string "ab\ncde\nfg\nh" |> lines |> List.map to_string = ["ab"; "cde"; "fg"; "h"]
 *)
-
-(** {2 Memory-map} *)
-
-let map_file_descr ?pos ?(shared=false) fd len =
-  B.map_file fd ?pos Bigarray.char Bigarray.c_layout shared len
-
-let with_map_file ?pos ?len ?(mode=0o644) ?(flags=[Open_rdonly]) ?shared name f =
-  let ic = open_in_gen flags mode name in
-  let len = match len with
-    | None -> in_channel_length ic
-    | Some n -> n
-  in
-  let a = map_file_descr ?pos ?shared (Unix.descr_of_in_channel ic) len in
-  try
-    let x = f a in
-    close_in ic;
-    x
-  with e ->
-    close_in ic;
-    raise e
